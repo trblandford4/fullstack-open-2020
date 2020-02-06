5368a5a970b6a0d237d4ed78667a6b1639da87b5
Ray Blandford <rayb@microsoft.com>
2020-02-05 17:49:57 -0800

    complete 1.2

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import Part from "./Part";
+
 const Content = ({courses}) => {
-  return courses.map(course => <p> {course.part} {course.exercises} </p>);
+  return courses.map(course => <Part course={course} />);
 }
  
  export default Content;
---
+++ part1/courseinfo/src/components/Part.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Part = ({course}) => {
+  return <p> {course.part} {course.exercises} </p>;
+}
+ 
+ export default Part;
+ 
